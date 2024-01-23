af6db9aae82dafff4d003ca2945ce4d015628eff
albert Kipchirchir <albertkipchirchir02@gmail.com>
2024-01-23 21:33:01 +0300

    project one day one

---
+++ main.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const app = express();
+// app.set("view engine", "ejs");
+
+const mysql = require("mysql");
+const myconn = mysql.createConnection({
+  host: "localhost",
+  database: "people_db",
+  user: "root",
+  password: "",
+});
+// test myconn
+myconn.connect((err) => {
+  if (!err) {
+    console.log("Database connection successful");
+  } else {
+    console.log(err);
+  }
+});
+
+app.get("/", (req, res) => {
+  res.send("Home Route/page");
+});
+app.get("/users", (req, res) => {
+  // fetch all users from db
+  myconn.query("SELECT * FROM users", (error, queryResult) => {
+    if (error) {
+      res.send(`A databse Error occured: ${error.message}`);
+    } else {
+      //   res.send(`A list of all users: ${JSON.stringify(queryResult)} `);
+    //   res.json(queryResult);
+        res.render("user.ejs", { queryResult });
+    }
+  });
+});
+app.post("/newuser", (req, res) => {
+  // save new user in db
+  res.redirect("/users");
+});
+
+app.listen(3003, () => console.log("App running on port 3003"));
